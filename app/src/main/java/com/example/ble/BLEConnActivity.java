33fd14533069e2c269233d0a4949e4b8bb401812
김창배 <cbkim93@naver.com>
2021-11-28 23:01:52 +0900

    BLE 통신 초기 틀 제작

---
+++ app/src/main/java/com/example/ble/BLEConnActivity.java
@@ -0,0 +1,14 @@
+package com.example.ble;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BLEConnActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ble_conn);
+    }
+}
--- app/src/main/java/com/example/ble/MainActivity.java
+++ app/src/main/java/com/example/ble/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.ble;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    final static String TAG = "MainActivity";
+    
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this,BLEConnActivity.class);
+        
+        Button btnBLEConnect = findViewById(R.id.btnBLEConnect);
+        btnBLEConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG,"블루투스 연결 화면 이동");
+                startActivity(intent);
+            }
+        });
     }
 }
